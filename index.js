6c18d2e5815247da2c50b0a5171242a1f5384bfd
Ajeet Singh <ajeetsinghjb@gmail.com>
2026-05-20 08:53:46 +0530

    cors config changed from localhost to live server

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://examyug-dashboard-frontend.vercel.app",
     credentials:true
 }))
 
