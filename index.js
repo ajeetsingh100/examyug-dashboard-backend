927c0ce45dd990ee589f43e88acd842e033ce954
Ajeet Singh <ajeetsinghjb@gmail.com>
2026-06-13 12:03:42 +0530

    new server added

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:3000",'http://localhost:3001','https://examyug-online.vercel.app','https://examyug-dashboard-frontend.vercel.app','https://examyug-online-eta.vercel.app'],
+    origin:["http://localhost:3000",'http://localhost:3001','https://examyug-online.vercel.app','https://examyug-dashboard-frontend.vercel.app','https://examyug-online-eta.vercel.app','http://localhost:5173'],
  
     credentials:true
 }))
